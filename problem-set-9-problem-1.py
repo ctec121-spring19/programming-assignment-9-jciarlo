aa4b77ef16f809beca5943b1842393fd34a81d61
Jon Ciarlo <j.ciarlo@students.clark.edu>
2019-05-20 14:55:57 -0700

    problems 1 and 4

--- problem-set-9-problem-1.py
+++ problem-set-9-problem-1.py
@@ -14,14 +14,32 @@ each letter with no spaces.
 
 IPO
 ==========
-INPUTS: 
-PROCESSES: 
-OUTPUTS: 
+INPUTS: users full name
+PROCESSES: use of variables and the split() method to get the intials of each name 
+OUTPUTS: users name in initial format 
 
 """
 
 def main():
-    pass # remove this line
-    # your code goes here
+
+    # get input from user
+
+    phrase = input("Enter your full name please: ")
+    # initialize variable to an empty string
+
+    initial1 = " "
+    # split the sentence into a list of words
+    # use a loop to iterate through the list of words
+
+    for word in phrase.split():
+        # concatenate the current value of acronym with the first
+        # letter from the current word being iterated
+        initial1 = initial1 + word[0]
+
+    # convert the entire acroynum string to upper case
+    initial1 = initial1.upper()
+   
+    # display the acroynum
+    print("The intials are:",initial1[1],".",initial1[2],".",initial1[3],".")
 
 main()
--- problem-set-9-problem-4.py
+++ problem-set-9-problem-4.py
@@ -42,14 +42,35 @@ morse_list = [' ', '--..--', '.-.-.-', '-----','.----', '..---', '...--',
                 '-.--', '--..']
 IPO
 ==========
-INPUTS: 
-PROCESSES: 
-OUTPUTS: 
+INPUTS: a sentence from the user
+PROCESSES: using the morse list, and the dictionary I'm going to be assigning character to numbers using a loop
+OUTPUTS: users name in morse code 
 
 """
 
 def main():
-    pass # remove this line
-    # your code goes here
-
+    #morse code to english 
+                #     ,         .         0       1        2        3
+    morse_list = [' ', '--..--', '.-.-.-', '-----','.----', '..---', '...--',
+                #4        5        6        7        8        9
+                '....-', '.....', '-....', '--...', '---..', '----.',
+                #A     B       C       D      E    F       G      H   
+                '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....',
+                #I     J       K      L       M     N     O      P
+                '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.',
+                #Q       R      S      T    U      V       W      X
+                '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-',
+                #Y       Z
+                '-.--', '--..']
+    #character dictionary 
+    char_dict = {' ':'0',',':'1','.':'2','0':'3','1':'4','2':'5','3':'6','4':'7','5':'8','6':'9','7':'10','8':'11','9':'12',
+                    'A':'13','B':'14','C':'15','D':'16','E':'17','F':'18','G':'19','H':'20','I':'21','J':'22','K':'23','L':'24','M':'25',
+                    'N':'26','O':'27','P':'28','Q':'29','R':'30','S':'31','T':'32','U':'33','V':'34','W':'35','X':'36','Y':'37','Z':'38'}
+    str1 = list(input("Enter a sentence: "))
+    #for loop 
+    for i in range(0, len(str1)):
+        char = str1[i]
+        charNum = char_dict[char.upper()]
+        morse = morse_list[int(charNum)]
+        print(morse, sep = "", end = "")
 main()
